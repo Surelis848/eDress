5dc71a91de8d0de9d36d0beacdf3da90c91ffcbe
Elena Eigelyte <elena.eigelyte@gmail.com>
2019-06-06 17:40:31 +0000

    Add initial functionality to filters

---
+++ static/js/filter.js
@@ -0,0 +1,13 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function typeFunction() {
+  document.getElementById("typeDropdown").classList.toggle("show");
+}
+
+function sizeFunction() {
+  document.getElementById("sizeDropdown").classList.toggle("show");
+}
+
+function colorFunction() {
+  document.getElementById("colorDropdown").classList.toggle("show");
+}
---
+++ static/js/load_more_button.js
@@ -0,0 +1,14 @@
+/*  $('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 3) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
+}); */
