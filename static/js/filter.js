a6280a3a8567134ce8901d09a082ee9b0cf09324
Elena Eigelyte <elena.eigelyte@gmail.com>
2019-09-04 07:56:02 +0000

    Change show class from add to toggle

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,15 +1,15 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function typeFunction() {
-  document.getElementById("typeDropdown").classList.add("show");
+  document.getElementById("typeDropdown").classList.toggle("show");
 }
 
 function sizeFunction() {
-  document.getElementById("sizeDropdown").classList.add("show");
+  document.getElementById("sizeDropdown").classList.toggle("show");
 }
 
 function colorFunction() {
-  document.getElementById("colorDropdown").classList.add("show");
+  document.getElementById("colorDropdown").classList.toggle("show");
 }
 
 /*When the user chooses the filter, hide the dropdown */
