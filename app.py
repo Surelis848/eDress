9e9f9c9cc19d4ddab9b30ecbfdf2532c3aa3ec1d
Elena Eigelyte <elena.eigelyte@gmail.com>
2019-09-19 14:55:37 +0000

    Change pagination variables

--- app.py
+++ app.py
@@ -19,19 +19,21 @@ else:
 mongo = PyMongo(app)
 clothes = mongo.db.clothes.find().sort('date', -1)
 
+
 def get_clothing(offset=0, per_page=10):
-    return clothes[offset: offset + per_page]
+    theclothes = mongo.db.clothes.find()
+    return theclothes[offset: offset + per_page]
 
 @app.route('/')
 @app.route('/get_clothes')
 def get_clothes():
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
-    total = clothes.count()
+    total = mongo.db.clothes.find().count()
     pagination_clothes = get_clothing(offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
-    return render_template("getclothes.html", 
+    return render_template("getclothes.html",
                                             clothes=clothes,
                                             types=mongo.db.types.find(),
                                             clothing=pagination_clothes,
@@ -40,7 +42,6 @@ def get_clothes():
                                             pagination=pagination,
                                             sizes=mongo.db.sizes.find(),
                                             colors=mongo.db.colors.find())
-
 #Get About page    
     
 @app.route('/get_about')
